7a2eb473fe435fb632c48fdd0f0909eca58dba6e
Romain Tello <romain.tello@gmail.com>
2019-12-02 15:44:41 +0100

    Initial commit

---
+++ function.py
@@ -0,0 +1,37 @@
+# Def = mot-clé qui permet de déclarer une fonction
+# addNumbers = nom de la fonction (libre, comme les noms de variables)
+# () = la ou les entrées, avec le nom de variable qu'on souhaite leur donner, séparées par une virgule
+# : = ouvrir un bloc
+def addNumbers(number1, number2):
+    # Début de la fonction, indentation
+
+    # return = renvoyer la valeur à l'endroit où on a appelé la fonction
+    return number1 + number2
+# Fin de la fonction, on retire l'indentation
+
+
+def isPositive(number):
+    # Si le nombre est inférieur à zéro
+    if number < 0:
+        # Renvoie faux
+        return False
+    # Sinon, si le nombre est supérieur à zéro
+    elif number > 0: 
+        # Renvoie vrai
+        return True
+    # Sinon
+    else:
+        # Renvoie None
+        return None
+
+
+def displayElements(list):
+    for element in list:
+        print(element)
+
+
+resultat = addNumbers(-6, addNumbers(2, 5))
+print("resultat =", resultat)
+print("resultat est-il positif?", isPositive(resultat))
+
+displayElements( [4, 12, 15, 37] )
---
+++ test.py
@@ -0,0 +1,18 @@
+# Snake case
+# On écrit les mots en minuscules séparés par un _
+# Cela fait l'image d'un serpent qui rampe
+nombre_entier = 3
+# Camel case
+# On écrit les mots attachés avec une majuscule au début de chaque mot
+# Cela fait l'image des bosses d'un chameau
+nombreEntier = 3    # Integer
+nombreReel = 1.0    # Float
+matin = True    # Boolean
+apresMidi = False
+text = "Ceci est une chaine de caractères"  # String
+liste1 = (4, 7, 12, 25)     # Tuple
+liste2 = [4, 7, 12, 25]     # List
+
+print(liste1[-1])
+
+print(nombreEntier + nombreReel)
