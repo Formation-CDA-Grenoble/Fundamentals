c50ca2bb83e37168d8c69da52e44ea5a8944ee72
Romain Tello <romain.tello@gmail.com>
2019-12-02 15:52:00 +0100

    Revert "Remove displayElements"
    This reverts commit 976e4291a85ca286aa9c78df2f409eac30f59dc4.

--- function.py
+++ function.py
@@ -25,6 +25,11 @@ def isPositive(number):
         return None
 
 
+def displayElements(list):
+    for element in list:
+        print(element)
+
+
 # Commentaires inutiles, mais importants à versionner quand même
 resultat = addNumbers(-6, addNumbers(2, 5))
 print("resultat =", resultat)
