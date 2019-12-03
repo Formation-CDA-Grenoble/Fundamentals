d3e9bc8e452a2a0769cc870e874a0b9ffb92462a
Romain Tello <romain.tello@gmail.com>
2019-12-03 12:24:24 +0100

    Add age functions

--- function.py
+++ function.py
@@ -30,9 +30,43 @@ def displayElements(list):
         print(element)
 
 
+def isLegal(age):
+    # Si l'âge est négatif
+    if age < 0:
+        # Arrête le programme et génère un message d'erreur
+        raise Exception("Age must be a positive integer")
+    # Sinon, si l'âge est supérieur à 18
+    elif age >= 18:
+        # Renvoie vrai
+        return True
+    # Sinon
+    else:
+        # Renvoie faux
+        return False
+
+
 # Commentaires inutiles, mais importants à versionner quand même
 resultat = addNumbers(-6, addNumbers(2, 5))
 print("resultat =", resultat)
 print("resultat est-il positif?", isPositive(resultat))
 
 displayElements( [4, 12, 15, 37, 56, 121] )
+
+
+def displayAges(ages):
+    # Pour chaque âge présent dans la liste
+    for age in ages:
+        # Afficher l'âge
+        print(age)
+        # Si cet âge ne correspond PAS à une personne majeure OU s'il est supérieur ou égal à 60 ans
+        if not isLegal(age) or age >= 60:
+            # Affiche "réduction"
+            print("réduction")
+        # Si cet âge ne correspond PAS à une personne majeure ET s'il est supérieur ou égal à 15 ans
+        if not isLegal(age) and age >= 15:
+            # Affiche "tarif spécial"
+            print("tarif spécial")
+
+
+displayAges( (15, 17, 21) )
+displayAges( (3, 12, 21, 15, 53, 62, 9, 0, 18, 75) )
